c792603ed2d60876ccd16027ab89f1dcae818c19
magic-onion <alexander.m.truong@gmail.com>
2018-10-09 18:09:33 -0400

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+require 'pry'
+class School
+  attr_accessor :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+  end
+
+
+  def add_student(student_name, grade)
+    if @roster[grade] == nil
+      self.roster[grade] = []
+      self.roster[grade] << student_name
+    else
+      self.roster[grade] << student_name
+    end
+  end
+
+  def grade(grade)
+    self.roster.each do |k,v|
+      if k == grade
+      return self.roster[k]
+    end
+  end
+end
+
+def sort
+  self.roster.each do |grade, student|
+    self.roster[grade] = student.sort
+  end
+end
+
+end
